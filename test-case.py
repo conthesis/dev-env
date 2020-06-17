fefd21e881ba337015da8cf8ea359621ee0fea7a
whn <me@whn.se>
2020-06-17 18:12:34 +0200

    update testcases

--- test-case.py
+++ test-case.py
@@ -8,12 +8,12 @@ http = httpx.AsyncClient()
 async def cas_store(data):
     if isinstance(data, dict):
         data = json.dumps(data)
-    resp = await http.post("http://cas.tspnhq.com/store", data=data)
+    resp = await http.post("http://cas.tspnhq.com/v1/store", data=data)
     resp.raise_for_status()
     return await resp.aread()
 
 async def insert_vsn(entity: str, pointer: bytes):
-    resp = await http.post("http://dcollect.tspnhq.com/entity-ptr/{entity}", data=pointer)
+    resp = await http.post(f"http://dcollect.tspnhq.com/entity-ptr/{entity}", data=pointer)
     resp.raise_for_status()
     return resp.json()
 
@@ -22,11 +22,11 @@ async def store_ent(entity: str, data):
     return await insert_vsn(entity, ptr)
 
 async def store_ents(ents):
-    asyncio.gather([store_ents(ent, val) for (ent, val) in ents])
+    await asyncio.gather(*[store_ent(ent, val) for (ent, val) in ents])
 
 
-async def subscribe(name, trigger_url, entities):
-    resp = await http.post("http://entwatcher.tspnhq.com/v1/subscribe/{name}", json=dict(trigger_url=trigger_url, entities=entities))
+async def subscribe(name):
+    resp = await http.post(f"http://entwatcher.tspnhq.com/v1/subscribe/{name}")
     resp.raise_for_status()
     return resp
 
@@ -36,16 +36,21 @@ async def main():
     await store_ents([
         ("foo", { "foo": 1}),
         ("bar", { "bar": 1}),
-        ("template", {"name":"foo","entries":[{"name":"step1","inputs":["a"],"command":{"kind":"identity"}}]}),
+        ("template", {"name":"foo","entries":[{"name":"step1","inputs":["a"],"action": "identity"}]}),
+        ("my_dag_watcher", {
+            "kind": "TriggerDAG",
+            "properties": {
+                "foo": "foo",
+                "bar": "bar",
+            }
+        })
     ])
 
-    await subscribe("my_dag", "TriggerDAG", {
-        "$Template": "template",
-        "foo": "foo",
-        "bar": "bar",
-    })
+    await subscribe("my_dag_watcher")
 
     await store_ents([
         ("foo", {"foo": 2}),
         ("bar", {"bar": 2}),
     ])
+
+asyncio.run(main())
