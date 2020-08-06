d0e8d23f7c21b49f2072f687fd90688f06b71b0c
whn <me@whn.se>
2020-08-06 22:32:56 +0200

    Update dev-env scripts for cfs

--- apply.py
+++ apply.py
@@ -7,26 +7,9 @@ from nats.aio.client import Client as NATS
 
 nc = NATS()
 
-async def cas_store(data):
-    if isinstance(data, dict):
-        data = json.dumps(data).encode("utf-8")
-    res = await nc.request("conthesis.cas.store", data, timeout=3)
-    if res.data is None or len(res.data) == 0:
-        return None
-    return res.data
-
-
-async def insert_vsn(entity: str, pointer: bytes):
-    data = entity.encode("utf-8") + b"\n" + pointer
-    res = await nc.request("conthesis.dcollect.store", data, timeout=3)
-    if res.data == b"ERR":
-        raise RuntimeError("dcollect.store failed")
-    return res.data
-
-
 async def store_ent(entity: str, data):
-    ptr = await cas_store(data)
-    return await insert_vsn(entity, ptr)
+    payload = entity.encode("utf-8") + b"\n" + json.dumps(data).encode("utf-8")
+    res = await nc.request("conthesis.cfs.put", payload)
 
 async def main():
     if len(sys.argv) != 3:
